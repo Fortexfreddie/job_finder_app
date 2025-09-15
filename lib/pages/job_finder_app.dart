cfa18ef731d769d9e8b9a831f4fb008a711674a2
Fortexfreddie <106601096+Fortexfreddie@users.noreply.github.com>
2025-09-15 18:43:32 +0100

    - Refactor main app widget into separate file for better organization

---
+++ lib/pages/job_finder_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class JobFinderApp extends StatefulWidget {
+  const JobFinderApp({super.key});
+
+  @override
+  State<JobFinderApp> createState() => _JobFinderAppState();
+}
+
+class _JobFinderAppState extends State<JobFinderApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
