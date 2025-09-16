8dafbe233abae314c1b014e2834b292093335176
Fortexfreddie <106601096+Fortexfreddie@users.noreply.github.com>
2025-09-16 11:35:40 +0100

    UI edits

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
-import 'pages/job_finder_app.dart';
+import 'screens/signup_page.dart';
 
 void main() {
   runApp(const JobFinderApp());
+}
+
+
+class JobFinderApp extends StatelessWidget {
+  const JobFinderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Job Finder',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SignUpPage(),
+    );
+  }
 }
---
+++ lib/screens/signup_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 80),
+                Center(
+                  child: Image(
+                    image: AssetImage("assets/image_1.1.png"),
+                  ),
+                ),
+              ],
+            ), 
+            ),
+        ),
+      ),
+    );
+  }
+}
